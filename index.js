a0e25e899a5adb7e47fecb86695ca4fe8cf25393
FarukKeskinsoy <133097328+codeaddev@users.noreply.github.com>
2023-12-26 14:28:40 +0300

    ...

---
+++ index.js
@@ -0,0 +1,62 @@
+import express from "express";
+import mysql from "mysql"
+import cors from "cors"
+
+const app=express()
+app.use(express.json())
+app.use(cors())
+const db= mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"recdo"
+})
+const rdb= mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"recdotest"
+})
+
+app.get("/",(req,res)=>{
+    res.json("Hello from sql backend")
+})
+
+app.get("/books",(req,res)=>{
+    const q = "SELECT * FROM books"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post("/books",(req,res)=>{
+    const q = "INSERT INTO books (`title`,`description`,`cover`) VALUES (?)"
+    const values =[
+        req.body.title,
+        req.body.description,
+        req.body.cover
+    ]
+    db.query(q,[values],(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+app.post("/gmstry",(req,res)=>{
+    const q = "INSERT INTO recdotest (`cariAdi`,`vknTckn`,`genel_toplam`) VALUES (?)"
+    const values =[
+        req.body.cariAdi,
+        req.body.vknTckn,
+        req.body.genel_toplam
+    ]
+    rdb.query(q,[values],(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+// if there is a auyh problem
+
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY "12345678"
+app.listen(process.env.PORT || 8800 ,()=>{
+    console.log("invoice running !")
+});
