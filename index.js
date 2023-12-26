fa7f752e0519ed4fc528816e51d6e2f469e2c412
FarukKeskinsoy <133097328+codeaddev@users.noreply.github.com>
2023-12-26 15:06:17 +0300

    ....

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const db= mysql.createConnection({
     database:"recdo"
 })
 const rdb= mysql.createConnection({
-    host:"DESKTOP-GPN9M4H\SQLEXPRESS",
+    host:"DESKTOP-GPN9M4H\\SQLEXPRESS",
     user:"mikrokom",
     password:"mikrokom2009/*-+",
     database:"RecDo"
